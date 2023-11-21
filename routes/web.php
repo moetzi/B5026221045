ac0ba72b12d47195f2d1f309116448cb45f780a8
moetzi <mutiaranoorfauzia@gmail.com>
2023-11-21 12:27:09 +0700

    Pertemuan 21-11-2023

--- routes/web.php
+++ routes/web.php
@@ -58,11 +58,24 @@ Route::get('pertemuan7', function () {
 });
 
 Route::get('profile', function () {
-    return view('index');
+    return view('index1');
 });
 
 Route::get('perkalian', 'App\Http\Controllers\DosenController@index');
 Route::get('biodata', 'App\Http\Controllers\DosenController@biodata');
+Route::get('showjam/{jam}', 'App\Http\Controllers\DosenController@showjam');
+
+Route::get('/formulir', 'App\Http\Controllers\DosenController@formulir');
+Route::post('/formulir/proses', 'App\Http\Controllers\DosenController@proses');
+
+//route CRUD
+Route::get('/pegawai','App\Http\Controllers\PegawaiController@index');
+Route::get('/pegawai/tambah','App\Http\Controllers\PegawaiController@tambah');
+Route::post('/pegawai/store','App\Http\Controllers\PegawaiController@store');
+Route::get('/pegawai/edit/{id}','App\Http\Controllers\PegawaiController@edit');
+Route::post('/pegawai/update','App\Http\Controllers\PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','App\Http\Controllers\PegawaiController@hapus');
+
 
 // pemanggilan sebuah library -> konsep OOP
 // kalo di php pake use dan ::
